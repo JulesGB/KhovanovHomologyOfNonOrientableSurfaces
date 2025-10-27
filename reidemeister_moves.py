71e16df61aec62d29a0d1e20377c617bf41762d7
Jules Goduco <jgoduco.bun@gmail.com>
2025-10-26 23:38:33 -0500

    R1 fixes for arc selection

--- reidemeister_moves.py
+++ reidemeister_moves.py
@@ -1,19 +1,28 @@
 import sage.knots
 import regina.engine
 
-# Create a new link with a left/right R1 move on a given strand
-def R1(link, strand_idx, sign=1):
+# R1: Create a new link with a left/right R1 move on a given strand
+# link : a link
+# arc_idx : idx indicating the arc of the given component
+# strand_idx : idx indicating component of link, default 0
+# sign : sign of new crossing (either -1 or 1), defaul 1
+def R1(link, arc_idx, strand_idx=0, sign=1):
     # convert to regina link
     rlink = regina.Link.fromPD(link.pd_code())
-    strand = rlink.component(strand_idx)
+
+    # locate the relevant arc in the component
+    arc = rlink.component(strand_idx)
+    for _ in range(arc_idx):
+        arc = arc.next()
     
     # do the move
-    # r1(strand, side of strand{0,1}, sign{-1,1})
-    if rlink.r1(strand, 0, sign):
+    # r1(arc, side of strand{0,1}, sign{-1,1})
+    if rlink.r1(arc, 0, sign):
         print('r1 performed')
     else:
         print('r1 unable to be performed')
-    
+
+    #convert back to sage link
     return sage.knots.link.Link(rlink.pdData())
 
 def R2(link, edge1, edge2):
