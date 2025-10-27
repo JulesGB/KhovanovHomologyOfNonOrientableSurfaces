0157f64a6d1f2e124323682a6c5b9d964f369459
Jules Goduco <jgoduco.bun@gmail.com>
2025-10-26 22:32:57 -0500

    implemented R1 moves with regina

--- reidemeister_moves.py
+++ reidemeister_moves.py
@@ -1,29 +1,22 @@
-from sage.knots.link import *
-import regina.engine.Link
+import sage.knots
+import regina.engine
 
-def R1(link, edge):
-    pd = link.pd_code().copy() # don't change input knot
+def R1(link, strand_idx):
+    # convert to regina link
+    rlink = regina.Link.fromPD(link.pd_code())
+    strand = rlink.component(strand_idx)
+    
 
-    # find total number of edges, i.e., the maximum edge label in pd
-    max_edge_label = max([item for row in pd for item in row])
-
-    # new edges alpha<beta
-    alpha, beta = max_edge_label+1, max_edge_label+2
-
-    # [+i, -beta, -alpha, +alpha]
-    pd.append([edge,beta,alpha,alpha])
-
-    # TODO - implement
-    print(pd)
-    return Link(pd)
-
-def R1_regina(link, edge):
-    pd = link.pd_code().copy()
-    link_regina = regina.Link.fromPD(pd)
-    link_regina.withR1(edge, 
+    # do the move
+    # r1(strand, side of strand{0,1}, sign{-1,1})
+    if rlink.r1(strand, 0, 1):
+        print('r1 performed')
+    else:
+        print('r1 unable to be performed')
+    
+    return sage.knots.link.Link(rlink.pdData())
 
 def R2(link, edge1, edge2):
-    pd = link.pd_code().copy()
 
     # TODO - implement
 
