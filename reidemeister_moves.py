f432c78040f64a4429a084353ee03d9ca40a34ce
Jules Goduco <jgoduco.bun@gmail.com>
2025-10-26 23:16:22 -0500

    code cleanup and optional R1 sign selection (l/r twist)

--- reidemeister_moves.py
+++ reidemeister_moves.py
@@ -1,14 +1,15 @@
 import sage.knots
 import regina.engine
 
-def R1(link, strand_idx):
+# Create a new link with a left/right R1 move on a given strand
+def R1(link, strand_idx, sign=1):
     # convert to regina link
     rlink = regina.Link.fromPD(link.pd_code())
     strand = rlink.component(strand_idx)
     
     # do the move
     # r1(strand, side of strand{0,1}, sign{-1,1})
-    if rlink.r1(strand, 0, 1):
+    if rlink.r1(strand, 0, sign):
         print('r1 performed')
     else:
         print('r1 unable to be performed')
