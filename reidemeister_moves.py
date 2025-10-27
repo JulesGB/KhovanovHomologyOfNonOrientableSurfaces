a0db7390f0df079097ed37496b6b6bd84b739c26
Jules Goduco <jgoduco.bun@gmail.com>
2025-10-26 20:33:49 -0500

    testing regina reidemeister implementations

--- reidemeister_moves.py
+++ reidemeister_moves.py
@@ -1,24 +1,36 @@
 from sage.knots.link import *
+import regina.engine.Link
 
 def R1(link, edge):
-    pd = link.pd_code()
+    pd = link.pd_code().copy() # don't change input knot
 
     # find total number of edges, i.e., the maximum edge label in pd
     max_edge_label = max([item for row in pd for item in row])
 
-    # TODO - implement
+    # new edges alpha<beta
+    alpha, beta = max_edge_label+1, max_edge_label+2
 
+    # [+i, -beta, -alpha, +alpha]
+    pd.append([edge,beta,alpha,alpha])
+
+    # TODO - implement
+    print(pd)
     return Link(pd)
 
-def R2(link, edge):
-    pd = link.pd_code()
+def R1_regina(link, edge):
+    pd = link.pd_code().copy()
+    link_regina = regina.Link.fromPD(pd)
+    link_regina.withR1(edge, 
+
+def R2(link, edge1, edge2):
+    pd = link.pd_code().copy()
 
     # TODO - implement
 
     return
 
-def R3(link, edge):
-    pd = link.pd_code()
+def R3(link, edge1, ):
+    pd = link.pd_code().copy()
 
     # TODO - implement
 
