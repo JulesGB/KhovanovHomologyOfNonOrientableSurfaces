5f27ecef6e443093fb873f1215b0ffd016b2db82
Jules Goduco <jgoduco.bun@gmail.com>
2025-10-26 22:34:56 -0500

    asdf

--- reidemeister_moves.py
+++ reidemeister_moves.py
@@ -6,7 +6,6 @@ def R1(link, strand_idx):
     rlink = regina.Link.fromPD(link.pd_code())
     strand = rlink.component(strand_idx)
     
-
     # do the move
     # r1(strand, side of strand{0,1}, sign{-1,1})
     if rlink.r1(strand, 0, 1):
@@ -23,7 +22,6 @@ def R2(link, edge1, edge2):
     return
 
 def R3(link, edge1, ):
-    pd = link.pd_code().copy()
 
     # TODO - implement
 
