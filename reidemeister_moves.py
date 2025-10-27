cce24be6e05bb82a3c2cf78196fb1de5d972565f
Jules Goduco <jgoduco.bun@gmail.com>
2025-10-26 19:01:02 -0500

    reideimeister PD code scaffolding

---
+++ reidemeister_moves.py
@@ -0,0 +1,25 @@
+from sage.knots.link import *
+
+def R1(link, edge):
+    pd = link.pd_code()
+
+    # find total number of edges, i.e., the maximum edge label in pd
+    max_edge_label = max([item for row in pd for item in row])
+
+    # TODO - implement
+
+    return Link(pd)
+
+def R2(link, edge):
+    pd = link.pd_code()
+
+    # TODO - implement
+
+    return
+
+def R3(link, edge):
+    pd = link.pd_code()
+
+    # TODO - implement
+
+    return
